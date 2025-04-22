fa6fc82a9a986fe097370eb7e30af521eda90e57
Jose-silvaa <jvitorfacanha@gmail.com>
2025-04-22 17:11:35 -0300

    User model

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,15 @@
 'use client'
 
-
-import {useEffect} from "react";
-import {testConnection} from "@/utils/connectionFirebase";
-import {createTestDoc} from "@/utils/createTestdoc";
-
 export default function Home() {
 
-  useEffect(()=> {
+  /*useEffect(()=> {
     testConnection().then(r => console.log(r));
   }, [])
 
   useEffect(()=> {
     createTestDoc();;
-  }, [])
+  }, [])*/
 
-
- 
   return (
       <h1>Vasco</h1>
   );
